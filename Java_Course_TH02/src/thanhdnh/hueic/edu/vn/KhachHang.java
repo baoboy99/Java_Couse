5e9666ee8fdf757146192e9af9766783adba171a
baoboy99 <37949672+baoboy99@users.noreply.github.com>
2018-04-21 17:00:42 +0700

    Update KhachHang.java

--- Java_Course_TH02/src/thanhdnh/hueic/edu/vn/KhachHang.java
+++ Java_Course_TH02/src/thanhdnh/hueic/edu/vn/KhachHang.java
@@ -13,3 +13,42 @@ public class KhachHang {
 	}
 	//Tiếp tục cho các phương thức khác
 }
+private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String usn) {
+		username = usn;
+	}
+	//Tiếp tục cho các phương thức
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String pw) {
+		password = pw;
+	}
+
+	public KhachHang(String usn, String pw) {
+		username = usn;
+		password = pw;
+	}
+
+	public KhachHang() {
+		username = "admin";
+		password = "admin";
+	}
+
+	public void MuaHang(String th, String nsx, double dg, int sl) {
+			HangHoa hh = new HangHoa();
+			hh.setTenHang(th);
+			hh.setNgaySanXuat(nsx);
+			hh.setDonGia(dg);
+			hh.setSoLuong(sl);
+			hh.TinhTien();
+			System.out.println(hh.toString());
+		}
+}
